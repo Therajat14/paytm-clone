2e00983502c9901a72f250fd660beb799a2a97e1
therajat14 <heylegend14@gmail.com>
2026-03-12 23:57:25 +0530

    inital commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,43 @@
 const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+app.use(bodyParser.json());
+app.use(cors());
+const port = 3000;
 
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://therajat14:therajat14@therajat14.yma6r.mongodb.net/paytm",
+  );
+  console.log("connected");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/signUp", (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
